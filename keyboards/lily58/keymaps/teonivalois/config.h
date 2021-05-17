14ac975e367f3a6fcadcc9296f46bf2a50d0d8f4
Teoni Valois <teonivalois@gmail.com>
2021-05-17 14:28:01 +0200

    first batch of customization

--- keyboards/lily58/keymaps/teonivalois/config.h
+++ keyboards/lily58/keymaps/teonivalois/config.h
@@ -27,7 +27,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
