41f098069af6417de90b94ee34a97ad23aacca6d
Teoni Valois <teonivalois@gmail.com>
2021-06-30 09:47:01 +0200

    moving around delete, underscore and fn2

--- keyboards/lily58/keymaps/teonivalois/keymap.c
+++ keyboards/lily58/keymaps/teonivalois/keymap.c
@@ -11,25 +11,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |BackSP|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|CMD+SPC|    |    +  |------+------+------+------+------+------|
+ * |------+------+------+------+------+------|   -   |    |    +  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |CMD+SPC| RGUI |
+ *                   |      |      |      |/       /         \      \ |      |       |      |
+ *                   `----------------------------'           '------''---------------------'
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+  KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LGUI(KC_SPC),                KC_EQL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-                        KC_LALT, KC_LGUI, MO(_LOWER), LGUI_T(KC_SPC), RSFT_T(KC_ENT), MO(_RAISE), KC_BSPC, KC_RGUI \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MINS,                KC_EQL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+                        KC_LALT, KC_LGUI, MO(_LOWER), LGUI_T(KC_SPC), RSFT_T(KC_ENT), MO(_RAISE), LGUI(KC_SPC), KC_RGUI \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -54,12 +54,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  |  _   |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  | DEL  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    | VOL+ | VOL- | HOME |  END |      |  |   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------.    ,-------| Left | Down |  Up  |Right |  :   |  "   |
- * |------+------+------+------+------+------|       |    |   +   |------+------+------+------+------+------|
+ * |------+------+------+------+------+------|   _   |    |   +   |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|   [  |   ]  |   <  |   >  |  ?   |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
@@ -68,10 +68,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, \
+  KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
   _______, _______, _______, _______, _______, _______,                   KC_VOLD, KC_VOLU, KC_HOME, KC_END,  _______, KC_PIPE, \
   _______, _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_COLN, KC_DQUO, \
-  _______, _______, _______, _______, _______, _______, _______, KC_PLUS, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   KC_QUES, _______, \
+  _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   KC_QUES, _______, \
                              _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 /* ADJUST
