b9c4dddd95f70f9c258062b2aa470b39b4dd80a3
Teoni Valois <teonivalois@gmail.com>
2022-01-21 11:48:32 +0100

    Update config.h
    defining the left side as the master and lowering the tapping term

--- keyboards/crkbd/keymaps/teonivalois/config.h
+++ keyboards/crkbd/keymaps/teonivalois/config.h
@@ -24,14 +24,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-// #define MASTER_LEFT
-#define MASTER_RIGHT
+#define MASTER_LEFT
+// #define MASTER_RIGHT
 // #define EE_HANDS
 
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
+#define TAPPING_TERM 100
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
